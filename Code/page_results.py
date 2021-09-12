3c2d7e9ba21eaa9a9af1f9a8afd6a3f185f526d8
ricgu8086 <ricgu8086@gmail.com>
2021-09-12 11:05:39 +0200

    Created logit to perform inference. Not working yet

--- Code/page_results.py
+++ Code/page_results.py
@@ -1,9 +1,48 @@
 import streamlit as st
 
-def results():
+def user_interface():
 
     text =  """
-            Hola, soy un resultado.
+            # Y estas son las respuestas
+            ¿Habremos tenido suerte?
             """
 
     st.write(text)
+
+    text = "Calcular resultados"
+    pressed = st.button(text)
+
+    return pressed
+
+
+def logic(result):
+
+    st.write(result.keys())
+
+    nlp = result['3. Descargar un modelo']
+    context = result['4. Contexto']
+    questions = result['5. Preguntas']
+
+    # Debug
+    st.write(nlp)
+    st.write(context)
+    st.write(questions)
+
+    for question in questions:
+
+        inp = {
+                  "question": question,
+                  "context": context
+              }
+
+        res = nlp(inp)
+
+        st.write(res)
+
+
+def results(result):
+
+    pressed = user_interface()
+
+    if pressed:
+        logic(result)
