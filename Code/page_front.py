868b615899f1c40ef7d46fb9cee646952b08f082
ricgu8086 <ricgu8086@gmail.com>
2021-09-18 20:46:56 +0200

    Reformated front page.

--- Code/page_front.py
+++ Code/page_front.py
@@ -2,16 +2,24 @@ import streamlit as st
 
 def front():
 
-    image_huggingface_url = "https://avatars.githubusercontent.com/u/25720743?s=200"
-
     text =  """
-            # Question Answering
+            # ¡Oh, un modelo de ML, vamos a desplegarlo! - Machine learning para adultos 
+            
+            ¡Enhorabuena por llegar a uno de los talleres más molones de la PyConEs 2021.!
+            
+            Hoy vas a trastear con:
 
-            NLP con transformers
-
-            PyConEs 2021
+            1. NLP, que son las siglas de Natural Language Processing. No tiene nada que ver con Neuro-Linguistic Programming.
+            2. Arquitecturas de redes neurolanes basadas en Transformers (usando la famosa librería HuggingFace).
+            3. Resolviendo la tarea conocida como "**Extractive Question Answering**".
             """
-
-
     st.write(text)
+
+
+    image_huggingface_url = "https://avatars.githubusercontent.com/u/25720743?s=200"
     st.image(image_huggingface_url)
+
+    text = "HuggingFace"
+    url = "https://huggingface.co/"
+    link = "[%s](%s)" % (text, url)
+    st.write(link)
