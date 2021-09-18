ad4de8a83ce32aecb388e04657fa993cf6826a33
ricgu8086 <ricgu8086@gmail.com>
2021-09-18 20:56:39 +0200

    Updated with Gandalf's image.

--- Code/page_your_turn.py
+++ Code/page_your_turn.py
@@ -8,8 +8,8 @@ def your_turn():
 
     st.write(text)
 
-    st.image("http://www.diariocolatino.com/wp-content/uploads/2021/04/Especial.jpg")
-    url2 = "https://i.pinimg.com/originals/3b/b1/c5/3bb1c58b6e6af93d26a60ec292da98b6.jpg"
+    url_gandalf_uncle_sam = "https://i.pinimg.com/originals/3b/b1/c5/3bb1c58b6e6af93d26a60ec292da98b6.jpg"
+    st.image(url_gandalf_uncle_sam)
 
 
 
