25cbc872d080b65e00f4ca200e63a7131e430607
impalah <impalah@gmail.com>
2021-09-19 17:07:51 +0200

    Cleaning (four)

---
+++ src/page_download_model.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+def user_interface():
+
+    text =  """
+            # Nos descargamos un modelo de Machine Learning que pueda hacer frente a esta tarea
+            """
+    st.write(text)
+    
+    defaul_model_name = ""
+    model_name = "Escoge el modelo que quieres usar"
+    user_input = st.text_input(model_name, defaul_model_name)
+    
+    text = """
+           Te recomendamos este: 
+           
+           deepset/roberta-base-squad2
+
+           Más información aquí: https://huggingface.co/deepset/roberta-base-squad2
+           """
+    st.write(text)
+
+    return user_input
+
+
+def logic(user_input):
+
+    from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+    
+    try:
+        nlp = pipeline("question-answering", model=user_input, tokenizer=user_input)
+        text = "Modelo correctamente descargado"
+        st.write(text)
+
+        st.session_state['nlp_model'] = nlp
+
+    except OSError:
+            text = "Modelo no encontrado"
+            st.write(text)
+
+def download_model():
+
+    user_input = user_interface()
+
+    if user_input:
+
+            text = "Descargar modelo"
+            pressed = st.button(text)
+
+            if pressed:
+                return logic(user_input)
---
+++ src/page_results.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+
+def user_interface():
+
+    text =  """
+            # Y estas son las respuestas
+            ¿Habremos tenido suerte?
+            """
+
+    st.write(text)
+
+    text = "Calcular resultados"
+    pressed = st.button(text)
+
+    return pressed
+
+
+def logic():
+
+    try:
+        nlp = st.session_state['nlp_model']
+        context = st.session_state['context_text']
+        questions = st.session_state['questions_text']
+
+    except KeyError:
+
+        text =  """
+                Completa los pasos en orden, porfi.
+                
+                Necesitamos configurar un modelo, unas preguntas y un contexto antes de seguir.
+                """
+        st.write(text)
+        return
+
+    results = []
+
+    for question in questions:
+
+        inp = {
+                  "question": question,
+                  "context": context
+              }
+
+        res = nlp(inp)
+
+        results.append(res)
+
+    # Formatting results
+    results_df = pd.DataFrame(results)
+    results_df['question'] = questions
+    selected_cols = ['score', 'question', 'answer']
+    results_df = results_df[selected_cols]
+
+    st.dataframe(results_df)
+
+    # For manual inspection
+    text = "Texto de la intranet"
+    expander = st.expander(text)
+    expander.write(context)
+
+
+def results():
+
+    pressed = user_interface()
+
+    if pressed:
+        logic()
